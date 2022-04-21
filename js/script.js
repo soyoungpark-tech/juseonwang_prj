d35dd9a2f572c4c9befdc6446f29c5e1b93ee8e1
parksoyoung <ps.young149@gmail.com>
2022-04-21 17:21:36 +0900

    add form
    add shape, alcohol, religion form and checkbox script

--- js/script.js
+++ js/script.js
@@ -5,3 +5,55 @@ $(document).ready(function() {
     }
 
 });
+// 체형 체크박스
+function count_ck(obj){
+    var chkbox = document.getElementsByName("shape");
+    // var chkbox = document.getElementsByName("alcohol");
+    var chkCnt = 0;
+
+    for(var i=0;i<chkbox.length; i++){
+        if(chkbox[i].checked){
+            chkCnt++;
+        }
+    }
+    // if(chkCnt>2){
+    if(chkCnt>1){
+        alert("check NO");
+        obj.checked = false;
+        return false;
+    }
+}
+// 주량 체크박스
+function count_ckAl(obj){
+    var chkboxAl = document.getElementsByName("alcohol");
+    var chkCntAl = 0;
+
+    for(var i=0;i<chkboxAl.length; i++){
+        if(chkboxAl[i].checked){
+            chkCntAl++;
+        }
+    }
+    // if(chkCntAl>2){
+    if(chkCntAl>1){
+        alert("check NO");
+        obj.checked = false;
+        return false;
+    }
+}
+// 종교 체크박스
+function count_ckRe(obj){
+    var chkboxRe = document.getElementsByName("religion");
+    var chkCntRe = 0;
+
+    for(var i=0;i<chkboxRe.length; i++){
+        if(chkboxRe[i].checked){
+            chkCntRe++;
+        }
+    }
+    // if(chkCntRe>2){
+    if(chkCntRe>1){
+        alert("check NO");
+        obj.checked = false;
+        return false;
+    }
+}
